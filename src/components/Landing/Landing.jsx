3acf4c0c82973f5b143d75cf0b47d0d5e0966a49
Fadem300 <fadel.m200@gmail.com>
2024-09-07 12:41:59 +0300

    added landing page and css for the landing page also added Responsive design for the landing and nav , footer

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,14 +1,41 @@
+import './Landing.css'
 
 const Landing = () => {
   return (
-    <main>
-      <h1>Hello, you are on the landing page for visitors.</h1>
-    <div className="contener">
+    <main >
+      <div className="LandingPageMain">
+      
+      
+    <div className="LandingPagecontener">
+        <h1>Hello, you are on the landing page for visitors.</h1>
+    
+    <div className='card'>
+      <div className='topCard'></div>
+      <div className='dawnCard'></div>
+    </div>
 
+    <div className='card'>
+    <div className='topCard'></div>
+    <div className='dawnCard'></div>
+    </div>
+
+    <div className='card'>
+    <div className='topCard'></div>
+    <div className='dawnCard'></div>
+    </div>
+    <div className='card'>
+    <div className='topCard'></div>
+    <div className='dawnCard'></div>
+    </div>
       
     </div>
 
 
+
+
+
+
+</div>
     </main>
   );
 };
