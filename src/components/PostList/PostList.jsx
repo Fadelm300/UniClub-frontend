178fcc77ad98a8ab638aae45291aaedc28f49481
Fadem300 <fadel.m200@gmail.com>
2024-09-26 10:52:00 +0300

    upgrade in some parts in navnar as sid bar and fixing the post details and user profile also the file list and landing page

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -6,16 +6,25 @@ const PostList = (props) => {
 
   const sortedPosts = [...props.posts].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
+  const MAX_TEXT_LENGTH = 200; 
+
   return (
     <>
       {sortedPosts.map((post, idx) => {
         const postDate = new Date(post.createdAt);
+        const isLongText = post.text.length > MAX_TEXT_LENGTH;
+        const truncatedText = isLongText ? post.text.slice(0, MAX_TEXT_LENGTH) + '...' : post.text;
 
         return (
           <Link key={idx} to={`${props.path}/post/${post._id}`}>
             <div className="card">
               <div className="topCard">
-                <div className="topcardtex">{post.text}</div>
+                <div className="topcardtex">
+                  {truncatedText}
+                  {isLongText && (
+                    <span className="readMore"> Click to read more</span>
+                  )}
+                </div>
               </div>
               <div className="dawnCard">
                 <div className="dawncardusername">{post.user.username}</div>
